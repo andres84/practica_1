3324675cfeef87882460bbeabda50571a0cdb159
SIL <SIL@SIL-PC>
2017-07-11 11:29:41 -0500

    Version 2 creacion de las clases entidades y de la unidad de persistencia - configuracion de las clases con sus anotaciones y relaciones

---
+++ practica_1/src/main/java/com/practica_1/modelo/Departamento.java
@@ -0,0 +1,88 @@
+package com.practica_1.modelo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "departamento")
+public class Departamento implements Serializable {
+
+    @Id
+    private int codigo_departamento;
+
+    @Column(name = "nombre_departamento")
+    private String nombre_departamento;
+
+    @OneToMany(mappedBy = "departamento", cascade = CascadeType.PERSIST)
+    List<Empleado> empleados = new ArrayList<>();
+
+    public Departamento() {
+    }
+
+    public Departamento(int codigo_departamento, String nombre_departamento) {
+        this.codigo_departamento = codigo_departamento;
+        this.nombre_departamento = nombre_departamento;
+    }
+
+    public int getCodigo_departamento() {
+        return codigo_departamento;
+    }
+
+    public void setCodigo_departamento(int codigo_departamento) {
+        this.codigo_departamento = codigo_departamento;
+    }
+
+    public String getNombre_departamento() {
+        return nombre_departamento;
+    }
+
+    public void setNombre_departamento(String nombre_departamento) {
+        this.nombre_departamento = nombre_departamento;
+    }
+
+    public List<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(List<Empleado> empleados) {
+        this.empleados = empleados;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + this.codigo_departamento;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Departamento other = (Departamento) obj;
+        if (this.codigo_departamento != other.codigo_departamento) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Departamento{" + "codigo_departamento=" + codigo_departamento + ", nombre_departamento=" + nombre_departamento + ", empleados=" + empleados + '}';
+    }
+
+}
